f58d22dc7021114840d1b8da84ceebb81e9c195d
Leonardo Kruger <kruger.leob@gmail.com>
2022-05-06 13:49:33 -0300

    fix path

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,14 @@ public class Main {
         Path path = Path.getInstance(System.getProperty("user.dir") + "/config/");
         Initialization.createFolder();
         Scanner scan = new Scanner(System.in);  // Create a Scanner object
+        System.out.println(ConsoleColors.stringColor("Historico deve estar em: ",ConsoleColors.CYAN) + "( PATH:"+ path.getPath() + "historico.csv )" );
+        System.out.println(ConsoleColors.stringColor("Grade curricular deve estar em: ",ConsoleColors.CYAN) + "( PATH:"+ path.getPath() + "grade.csv )" );
         System.out.println(ConsoleColors.stringColor("Informações básicas sobre o aluno são: Nome, GRR (int), curso, telefone (int), email",ConsoleColors.CYAN));
         System.out.println(ConsoleColors.stringColor("Deseja inserir as informçãoes básicas manualmente ou carregar de csv? Digite", ConsoleColors.CYAN)
                 + ConsoleColors.stringColor(" m ",ConsoleColors.RED)
                 + ConsoleColors.stringColor("para manual ou",ConsoleColors.CYAN)
                 + ConsoleColors.stringColor(" c ",ConsoleColors.RED)
-                + ConsoleColors.stringColor("para CSV ",ConsoleColors.CYAN) + "( PATH:"+ path + "historico.csv )" );
+                + ConsoleColors.stringColor("para CSV ",ConsoleColors.CYAN) + "( PATH:"+ path.getPath() + "user.csv )" );
         System.out.println(ConsoleColors.stringColor("Após inicialização serão impressos informações sobre historico, grade curricular e outros.", ConsoleColors.CYAN));
 
 
@@ -54,7 +56,7 @@ public class Main {
         user.desempenhoSemestre("22/01");
         user.imprimeDesempenho();
 
-        System.out.println(ConsoleColors.stringColor("\nDeseja importar um pedido existente? (path: config/pedido.json):",ConsoleColors.CYAN)
+        System.out.println(ConsoleColors.stringColor("\nDeseja importar um pedido existente?",ConsoleColors.CYAN) + "( PATH:" + path.getPath() + "pedido.json):"
                 + ConsoleColors.stringColor(" s ",ConsoleColors.RED)
                 + ConsoleColors.stringColor("ou",ConsoleColors.CYAN) + ConsoleColors.stringColor(" n ", ConsoleColors.RED)
                 + ConsoleColors.stringColor("(criar um novo)", ConsoleColors.CYAN));
@@ -83,9 +85,9 @@ public class Main {
             pedido.readSolicita(ofertadas);
         }
 
-        System.out.println(ConsoleColors.stringColor("Seu pedido esta finalizado e salvo em /config/pedido.json",ConsoleColors.CYAN));
+        System.out.println(ConsoleColors.stringColor("Seu pedido esta finalizado e salvo em ",ConsoleColors.CYAN) + "PATH:" + path.getPath() + "pedido.json");
         System.out.println(ConsoleColors.stringColor("Processo finalizado, escolha a opção que desejar:",ConsoleColors.CYAN));
-        System.out.printf(ConsoleColors.stringColor("Gerar arquivo de saida (path:/config/saida.txt): "));
+        System.out.printf(ConsoleColors.stringColor("Gerar arquivo de saida ") + "PATH:" + path.getPath() + "saida.txt" );
         System.out.printf(ConsoleColors.stringColor("1\n",ConsoleColors.RED));
         System.out.printf(ConsoleColors.stringColor("Editar materias solicitadas: "));
         System.out.printf(ConsoleColors.stringColor("2\n",ConsoleColors.RED));
@@ -101,7 +103,7 @@ public class Main {
                 System.out.println(ConsoleColors.stringColor("Valor invalido",ConsoleColors.RED));
 
             System.out.println(ConsoleColors.stringColor("Processo finalizado, escolha a opção que desejar:",ConsoleColors.CYAN));
-            System.out.printf(ConsoleColors.stringColor("Gerar arquivo de saida (path:/config/saida.txt): "));
+            System.out.printf(ConsoleColors.stringColor("Gerar arquivo de saida ")+ "PATH:" + path.getPath() + "saida.txt" );
             System.out.printf(ConsoleColors.stringColor("1\n",ConsoleColors.RED));
             System.out.printf(ConsoleColors.stringColor("Editar materias solicitações: "));
             System.out.printf(ConsoleColors.stringColor("2\n",ConsoleColors.RED));
